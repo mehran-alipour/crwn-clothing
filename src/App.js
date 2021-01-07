0ba837e4f486e75512654d1dc267f074bfe3e8de
Mehran Alipour <malipour7@myseneca.ca>
2021-01-07 01:03:16 -0500

    added sign-in-and=sign-out-page, added sign-in component, form component and button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './compomemts/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const ErrorPage = () => (
   <div>
@@ -18,7 +19,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         <Route path='/*' component={ErrorPage} />
       </Switch>
     </div>
