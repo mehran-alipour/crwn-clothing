50a8e1cb5536af5016899c9947a76c29084175cd
Mehran Alipour <malipour7@myseneca.ca>
2021-01-13 16:39:35 -0500

    redux included and used for user singin and signout page, update header to reflect new changes, create cart-icon componetn and add to header component

--- src/compomemts/header/header.component.jsx
+++ src/compomemts/header/header.component.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import {auth} from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
-import {ReactComponent as Logo} from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
-
-const Header = ({currentUser}) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className='logo'/>
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
@@ -19,18 +21,22 @@ const Header = ({currentUser}) => (
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
-      {
-        currentUser ? 
-        <div className="option" onClick={()=> auth.signOut()}>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
-        :
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      }
+      ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon/>
     </div>
   </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
